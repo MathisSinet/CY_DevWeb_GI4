b6dba66a2cb44693931fa5e8062e8a731277f6ea
MatteoVRN <134708279+MatteoVRN@users.noreply.github.com>
2026-04-06 12:44:09 +0200

    Update views.py

--- core/views.py
+++ core/views.py
@@ -6,3 +6,21 @@ def test(request):
 
 def index(request):
     return render(request, "index.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def concept(request):
+    return render(request,"concept.html")
+
+def boutique(request):
+    return render(request,"boutique.html")
+
+def garderie(request):
+    return render(request,"garderie.html")
+
+def cart(request):
+    return render(request,"cart.html")
+
+def search(request):
+    return render(request,"search.html")
