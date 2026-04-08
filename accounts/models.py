7605917a1eafdeaacfd4afc9f120fcab262183d8
MathisSinet <mathis.sinet@gmail.com>
2026-04-08 16:10:42 +0200

    Mise à jour du formulaire d'inscription

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     points = models.IntegerField(default=0)
     verified = models.BooleanField(default=False)
