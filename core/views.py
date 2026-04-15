6e1647f4385e88facd650c31bb64adf222f94b2a
MathisSinet <mathis.sinet@gmail.com>
2026-04-15 14:40:17 +0200

    Corrections

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ObjetConnecte
 from accounts.utils import addPoints
